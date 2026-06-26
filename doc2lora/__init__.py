849a6f5314929b134e29d9f99655f6935dd29e85
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2026-06-26 18:39:08 -0500

    chore: bump version

--- doc2lora/__init__.py
+++ doc2lora/__init__.py
@@ -3,5 +3,5 @@
 from .core import convert, convert_from_r2
 
 # single source of truth for the package version (read by pyproject + cli)
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __all__ = ["convert", "convert_from_r2", "__version__"]
